93e8437cbbdc938596421350d8b8662671e748e3
aarushig25 <aarushidtu@gmail.com>
2017-04-07 21:56:52 +0530

    format and done js

--- done.js
+++ done.js
@@ -2,47 +2,41 @@
  * Created by a.gupta1409 on 07-04-2017.
  */
 
-
+/* function to be implemented on button DONE */
 function done(){
-    console.log("doIT");
     var editor = document.getElementById('editor');
+
     var regex1 = /\<a\>/g;
     var regex2 = /\<\/a\>/g;
-    console.log(regex1);
-    console.log(regex2);
-    var arr1 = [];
-    var arr2 = [];
+
+    var arr1 = [];      /* arr1 holds index of <a> */
+    var arr2 = [];      /* arr2 holds index of </a> */
 
     var x;
 
     while(true){
         x = regex1.exec(editor.innerText);
-        console.log(x);
         if(x === null){
             break;
         }
         arr1.push(x.index)
     }
-    console.log("arr1");
-    console.log(arr1);
 
     var y;
 
     while(true){
         y = regex2.exec(editor.innerText);
-        console.log(y);
         if(y === null){
             break;
         }
         arr2.push(y.index)
     }
-    console.log("arr2");
-    console.log(arr2)
 
 
     var link = document.getElementById('link');
     link.innerHTML = "";
 
+    /* Adding links to the bottom of the page */
     arr1.every(function(item,index,arr1){
         if(arr2[index+1] < item || arr1[index+1] < arr2[index]){
             alert("anchor tags are not appropriate")
@@ -53,7 +47,6 @@ function done(){
         var a = document.createElement('a');
         a.href = "null";
         var str = editor.innerText.substr(item+3,arr2[index]-item-3);
-        console.log(str);
         var txt = document.createTextNode(str);
         if( (index+1)%2 === 0) {
             a.style.color = 'blue';
--- format.js
+++ format.js
@@ -4,7 +4,6 @@
 
 var tooltip = {
     show: function (e) {
-        console.log("show");
         var elem = document.createElement('span');
         elem.className = 'tooltip';
         elem.onblur = function () {
@@ -13,27 +12,29 @@ var tooltip = {
         addButton(elem);
         e.target.appendChild(elem);
         e.target._spanRef = elem;
-
     }
 }
 
+/* function to implement tooltip with formatting options */
 function formatting(e){
-    console.log("dblCLick");
-    tooltip.show(e);
+    if(window.getSelection().getRangeAt(0).toString()) {
+        tooltip.show(e);
+    }
 }
 
+/* function to add buttons to tooltip */
 function addButton(elem) {
 
     var editor = document.getElementById('editor');
 
     var div = document.createElement('div');
     div.className = 'btn-group';
+
+    /* BOLD */
     var bold = document.createElement('button');
     bold.innerHTML = 'B';
     bold.id = 'bold';
-
     bold.onclick = function (e) {
-        console.log("bold")
         editor.focus();
         document.execCommand("bold", false, null);
         document.querySelector('.tooltip').remove();
@@ -41,12 +42,11 @@ function addButton(elem) {
 
     div.appendChild(bold);
 
+    /* UNDERLINE */
     var underline = document.createElement('button');
     underline.innerHTML = 'U';
     underline.id = 'underline';
-
     underline.onclick = function (e) {
-        console.log("underline")
         editor.focus();
         document.execCommand('underline',false,null);
         document.querySelector('.tooltip').remove();
@@ -55,11 +55,11 @@ function addButton(elem) {
 
     div.appendChild(underline);
 
+    /* RED */
     var red = document.createElement('button');
     red.innerHTML = 'RED';
     red.id = 'red';
     red.onclick = function (e) {
-        console.log("red")
         editor.focus();
         document.execCommand('forecolor', false, 'red');
         document.querySelector('.tooltip').remove();
@@ -67,8 +67,16 @@ function addButton(elem) {
     }
     div.appendChild(red);
 
-
-
-
     elem.appendChild(div);
 }
+
+/* function to handle click outside tootltip */
+function handle(event) {
+    var id = event.srcElement.id;
+    var tooltip = document.querySelector('.tooltip');
+    if(id=='bold' || id=='underline' || id=='red'){
+    }else if(tooltip){
+        tooltip.remove();
+    }
+}
+
